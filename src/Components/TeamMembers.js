66601644c9f6c0ca8ea315f2ecbffbaa71b0d645
akshay pv <apvnambiar@gmail.com>
2021-10-09 11:58:36 +0530

    gagana profile pic added

--- src/Components/TeamMembers.js
+++ src/Components/TeamMembers.js
@@ -43,7 +43,7 @@ function TeamMembers() {
 
         <div className="profiles">
           <div className="pro-pic">
-            <img src="https://avatars.githubusercontent.com/u/85632108?v=4" class="profile-img" />
+            <img src="https://avatars.githubusercontent.com/u/85632108?v=4" className="profile-img" />
 
 
             <div className="profile-details">
@@ -56,7 +56,7 @@ function TeamMembers() {
 
         <div className="profiles">
           <div className="pro-pic">
-            <img src="https://avatars.githubusercontent.com/u/84629891?v=4" class="profile-img" />
+            <img src="https://avatars.githubusercontent.com/u/84629891?v=4" className="profile-img" />
 
             <div className="profile-details">
               <h3 className="profile-details1" >Akshay Nambiar PV </h3>
@@ -70,7 +70,7 @@ function TeamMembers() {
 
         <div className="profiles">
           <div className="pro-pic">
-            <img src="https://avatars.githubusercontent.com/u/48583694?v=4" class="profile-img" />
+            <img src="https://avatars.githubusercontent.com/u/48583694?v=4" className="profile-img" />
 
 
             <div className="profile-details">
@@ -83,7 +83,7 @@ function TeamMembers() {
 
         <div className="profiles">
           <div className="pro-pic">
-            <img src="https://ca.slack-edge.com/T0246ARE8UT-U024DHJ8KRC-26b8dc94c171-72" class="profile-img" />
+            <img src="https://ca.slack-edge.com/T0246ARE8UT-U024DHJ8KRC-26b8dc94c171-72" className="profile-img" />
 
 
             <div className="profile-details">
@@ -97,7 +97,7 @@ function TeamMembers() {
 
         <div className="profiles">
           <div className="pro-pic">
-            <img src="https://ca.slack-edge.com/T0246ARE8UT-U0255ERTF0V-a40de1056f78-512" class="profile-img" />
+            <img src="https://ca.slack-edge.com/T0246ARE8UT-U0255ERTF0V-a40de1056f78-512" className="profile-img" />
 
 
             <div className="profile-details">
@@ -110,7 +110,7 @@ function TeamMembers() {
 
         <div className="profiles">
           <div className="pro-pic">
-            <img src="https://lh3.googleusercontent.com/5RdjNR0AddkFHYGSlgt0hYO8ZvvLTXGP2ih4krfx-m4mxCsrWB-5pzofHoCUPBpRlbTn4L9u-S4rRbARX-GYFCVMawXeL5Xbx1v22rl8e_DUFIzUtF-WQNF9vofVyxtL0OkCzwGGBc84E3awFk1kaDuGXCADMU0f3LQLCbsY1ySycmWi64Jpjpn9Hsx7Oa_xLXIRA_TUMdMco6S2fno16eH3W3i1zzZhYeFjqtDJ36aBLcH3JlSbIIrN-0MiXIZoTWUbYEHn-RhI9rtIuqWLaG1Pp1O5-ntjt8ug53mqkGPEW94kopf6T2kgYa2LND2C_wbWl2AyD-j3xOlIjBJN0cOfXDqJuldyfDRWJuq0ptnpqhdQaoWEivfsE2ZL1IcmdxpPVPOl-JzuJ-qVDFs0MXl1Miql4pNvY7dhDd2hEXxJKWJTtMUviLPlOtGcWTCOSYefOhQJZ9-HxSsQwb4J8U-6_ONicuY4_V1RM7f54Dj8SLbXwXYLs6qwFH_SQcbeoUjZM8htBnmXHauMM-HF7V4Q6ZE7Ssorvzr9OPSe-ueDvoLYyBC6TbklCo6_i1nab4Dmz7Au8MVXFn4950ODjdW_KAMgi9fbPpXFJINdEd3LMe04lu1dMLaLs5zuwxIyEB7k1qYSdwqVBRV6ZNGFHFHF-1JEghVxz-MXQb6FSGJ1B6Geu2iA_eBc2aCFsWaChwYS5K1ioFY1NVQMCrdnCcU=w693-h923-no?authuser=2" class="profile-img" />
+            <img src="https://res.cloudinary.com/lanta/image/upload/v1633623597/gagananpic_ubhixq.jpg" className="profile-img" />
 
             <div className="profile-details">
               <h3 className="profile-details1" >Gagana Karpurachalil </h3>
@@ -124,7 +124,7 @@ function TeamMembers() {
 
         <div className="profiles">
           <div className="pro-pic">
-            <img src="https://avatars.githubusercontent.com/u/88938045?v=4" class="profile-img" />
+            <img src="https://avatars.githubusercontent.com/u/88938045?v=4" className="profile-img" />
 
 
             <div className="profile-details">
